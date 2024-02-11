99de09df16eece71a5b20a5e0e2e6dfd11d50aa3
nawfalel1 <elachar2001@gmail.com>
2024-02-12 00:56:57 +0100

    Working program

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿public class FindFile {
+
+
+    public static List<String> findFile(String directoryPath)
+    {
+        List<String> list = new List<string>();
+        if (Directory.Exists(directoryPath))
+        {
+
+            String[] files = Directory.GetFiles(directoryPath);
+
+            foreach (String file in files)
+            {
+                if (Path.GetFileName(file).StartsWith("Fab") && Path.GetFileName(file).EndsWith(".txt"))
+                {
+                    list.Add(file);
+                }
+            }
+
+        }
+        else
+        {
+            throw new Exception("Directory does not exist.");
+        }
+        return list;
+    }
+    static void Main() {
+        Console.WriteLine("Donnez le nom du dossier");
+        String directory = Console.ReadLine();
+        Console.WriteLine("\n");
+        List<String> list = findFile(directory);
+        
+        foreach(String file in list)
+        {
+            Console.WriteLine(file);
+        }
+    }
+    
+
+}
+
